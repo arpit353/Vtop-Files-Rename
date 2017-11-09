fb01a330efc47183d57914eb269ece18c6e8b248
arpit353 <arpit.khurana2015@gmail.com>
2017-11-09 11:45:07 +0530

    initial commit

---
+++ Vtop files Rename.py
@@ -0,0 +1,14 @@
+import glob
+import os
+
+file_names = []
+    
+for i in glob.glob("*.*"):
+
+	if(i == "rename.py"):
+		os.remove(i)
+
+    last =  i.rfind('Material')
+    print i[last+9:]
+    
+    os.rename(i,i[last+9:])
